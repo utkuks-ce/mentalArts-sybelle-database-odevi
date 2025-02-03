ca730360ac06d1059b2a33d220c7cc2c8555f674
Utku Kadir Somer <161475732+utkuks-ce@users.noreply.github.com>
2025-02-03 08:40:55 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo "📢 Docker Compose 1 başlatılıyor..."
+echo "Docker Compose 1 başlatılıyor..."
 docker compose -f docker-compose.yml up -d
 sleep 10  
 
-echo "⏳ WordPress'in tam olarak hazır olmasını bekliyoruz..."
+echo "WordPress'in tam olarak hazır olmasını bekliyoruz..."
 until $(curl --output /dev/null --silent --head --fail http://localhost:8080/wp-json/wp/v2/posts); do
     printf '.'
     sleep 5
 done
-echo "✅ WordPress API artık çalışıyor!"
+echo "WordPress API artık çalışıyor!"
 
-echo "📢 Docker Compose 2 başlatılıyor..."
+echo "Docker Compose 2 başlatılıyor..."
 docker compose -f docker-compose.yml -f docker-compose-2.yml up -d
 sleep 10  
 
-echo "✅ Veritabanları hazır!"
+echo "Veritabanları hazır!"
 echo "veritabanına ilk giriş yapılıyor"
 
-echo "📢 data_loader başlatılıyor..."
-docker start data_loader || echo "⚠️ data_loader çalıştırılamadı!"
+echo "data_loader başlatılıyor..."
+docker start data_loader || echo "data_loader çalıştırılamadı!"
 
-echo "📢 wp_updater başlatılıyor..."
-docker start wp_updater || echo "⚠️ wp_updater çalıştırılamadı!"
+echo "wp_updater başlatılıyor..."
+docker start wp_updater || echo "wp_updater çalıştırılamadı!"
 
-echo "✅ Tüm sistem başarıyla çalıştırıldı!"
+echo "Tüm sistem başarıyla çalıştırıldı!"
 
